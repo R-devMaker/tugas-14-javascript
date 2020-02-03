d2d9780a66a9857af840a6a28c703ffb0a9b3025
R-devMaker <rei.forworkonly@gmail.com>
2020-02-03 14:16:10 +0700

    Tugas 14 JavaScript
    Pengumpulan tugas 14 JavaScript Niomic

---
+++ tugas14.js
@@ -0,0 +1,9 @@
+function tugas14() {
+    var x = 37/31;
+
+    return x;
+}
+
+console.log("Hasil Ceil : ", Math.ceil(tugas14()));
+console.log("Hasil Floor : ", Math.floor(tugas14()));
+console.log("Hasil Round : ", Math.round(tugas14()));
